1759afcaaf98e03a987f616471f7abc9ed698400
CamilleChlc <camille.chaulaic@gmail.com>
2020-06-02 16:45:27 +0200

    Creation of html architecture and addition on style

--- main.js
+++ main.js
@@ -1,12 +1,50 @@
-function draw() {
-  var canevas = document.getElementById('canevas');
-  if (canevas.getContext) {
-    var ctx = canevas.getContext('2d');
-
-    ctx.beginPath();
-    ctx.moveTo(75, 50);
-    ctx.lineTo(100, 75);
-    ctx.lineTo(100, 25);
-    ctx.fill();
-  }
-}
+document.addEventListener("DOMContentLoaded", function() {
+	let projects = [
+		{ name: "Antivirus",
+			year: 2017,
+			description: "As part of the first year of university, we had to develop a board game in Ada.",
+			technologies : ["Ada"],
+			images: [""]
+		},
+		{ name: "Île Interdite",
+			year: 2017,
+			description: "As part of the first year of DUT Informatique and in teams of three, we had to design and develop a cooperative board game.",
+			technologies : ["Java"]
+		},
+		{ name: "Math'Scape World",
+			year: 2018,
+			description: "During the second year DUT Informatique and by team of 7 people, we imagined, designed and developed a mobile application to educate middle school students about mathematical and computer thinking.",
+			technologies : ["Java", "Android"]
+		},
+		{ name: "Ski race",
+			year: 2018,
+			description: "As part of the optional second year Graphics module, I created with two classmates an SVG animation on the theme of ski racing.",
+			technologies : ["SVG"]
+		},
+		{ name: "UnJeu",
+			year: 2018,
+			description: "For the Android course, I developed an application to revise mathematics and general cultures for an audience of middle school students.",
+			technologies : ["Java", "Android"]
+		},
+		{ name: "SpaceTeam",
+			year: 2020,
+			description: "During the Android workshop, we had to recreate the cooperative shouting game SpaceTeam, in teams of 3.",
+			technologies : ["Kotlin", "Android"]
+		},
+		{ name: "AuKrowdfunding",
+			year: 2020,
+			description: "As part of the PHP / Symfony course during the LP DIM, I created with two other people a crowdfunding platform in Symfony.",
+			technologies : ["PHP", "Symfony"]
+		},
+		{ name: "Movies Explorer",
+			year: 2020,
+			description: "During the development workshop under IOS, we had to create an application for viewing fact sheets on films, using the Movie Db API.",
+			technologies : ["Swift", "IOS"]
+		},
+		{ name: "Digital Immersive Museum",
+			year: 2020,
+			description: "As part of the PHP / Symfony course during the LP DIM, I created with two other people a crowdfunding platform in Symfony.",
+			technologies : ["Android", "Kotlin", "Eye tracking", "Javascript"]
+		}
+	]
+})
